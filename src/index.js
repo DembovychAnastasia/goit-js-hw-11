ff9e0d8dd0be3d0c0d133850924837856535a083
DembovychAnastasia <113368687+DembovychAnastasia@users.noreply.github.com>
2023-03-28 20:13:07 +0300

    update file

--- src/index.js
+++ src/index.js
@@ -1,108 +1,109 @@
-
 import axios from 'axios';
-import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
 
-// пошук елементів документа
-const refs = {
-  form: document.querySelector('.search-form'),
-  input: document.querySelector('input'),
-  gallery: document.querySelector('.gallery'),
-  btnLoadMore: document.querySelector('.load-more'),
-};
 
-let page = 1; // початкове значення параметра page повинно бути 1 сторніка
+const searchForm = document.querySelector('#search-form');
+const searchText = document.querySelector('input');
+const searchBtn = document.querySelector('.search-btn');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку
-refs.form.addEventListener('submit', onSearch); // слухач події на submit
-refs.btnLoadMore.addEventListener('click', onBtnLoadMore); // слухач події клік по кнопці loadMore
+let page = 1;
+let search = '';
+let images = [];
 
-// обробка події на submit
-function onSearch(evt) {
-  evt.preventDefault();
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-  page = 1;
-  refs.gallery.innerHTML = '';
+function fetchArticles(q, page) {
+  return axios.get('', {
+    params: {
+      q,
+      page,
+      per_page: 40,
+      key: '34601411-5a068c4760602afda1223d229',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+}
 
-  const name = refs.input.value.trim();
-
-  if (name !== '') {
-    pixabay(name);
-  } else {
-    refs.btnLoadMore.style.display = 'none';
-
+searchForm.addEventListener('submit', async () => {
+  event.preventDefault();
+  cleanGallery();
+  search = event.target[0].value;
+  if (!search) {
     return Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
-}
-
-// дії кнопки LoadMore
-function onBtnLoadMore() {
-  const name = refs.input.value.trim();
-  page += 1;
-  pixabay(name, page); // завантаження зображень
-}
-
-async function pixabay(name, page) {
-  const API_URL = 'https://pixabay.com/api/';
-
-  const options = {
-    params: {
-      key: '34601411-5a068c4760602afda1223d229',
-      q: name,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      page: page,
-      per_page: 40,
-    },
-  };
-
   try {
-    const response = await axios.get(API_URL, options);
-
-    notification(response.data.hits.length, response.data.total);
-
-    createMarkup(response.data);
+    const response = await fetchArticles(search, page);
+    if (!response.data.hits.length) {
+      throw new Error(
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        )
+      );
+    } else {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.total.hits} images.`
+      );
+    }
+    console.log(response);
+    images = response.data.hits;
+    appendItems(response.data.hits);
+    showBtn(response.data.totalHits);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+  }
+});
+
+function showBtn(totalHits) {
+  if (images.length < totalHits) {
+    loadMoreBtn.removeAttribute('hidden');
+  } else {
+    loadMoreBtn.setAttribute('hidden', true);
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
   }
 }
 
-function createMarkup(arr) {
-  const markup = arr.hits
+function appendItems(hits) {
+  const markup = hits
     .map(
       item =>
-        `<a class="photo-link" href="${item.largeImageURL}">
-            <div class="photo-card">
-            <div class="photo">
-            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
-            </div>
-                    <div class="info">
-                        <p class="info-item">
-                            <b>Likes</b>
-                            ${item.likes}
-                        </p>
-                        <p class="info-item">
-                            <b>Views</b>
-                            ${item.views}
-                        </p>
-                        <p class="info-item">
-                            <b>Comments</b>
-                            ${item.comments}
-                        </p>
-                        <p class="info-item">
-                            <b>Downloads</b>
-                            ${item.downloads}
-                        </p>
-                    </div>
-            </div>
-        </a>`
+      `<a class="photo-link" href="${item.largeImageURL}">
+                   <div class="photo-card">
+                   <div class="photo">
+                   <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+                   </div>
+                           <div class="info">
+                               <p class="info-item">
+                                   <b>Likes</b>
+                                   ${item.likes}
+                               </p>
+                               <p class="info-item">
+                                   <b>Views</b>
+                                   ${item.views}
+                             </p>
+                               <p class="info-item">
+                                   <b>Comments</b>
+                                   ${item.comments}
+                               </p>
+                         <p class="info-item">
+                                   <b>Downloads</b>
+                                   ${item.downloads}
+                               </p>
+                           </div>
+                   </div>
+             </a>`
     )
     .join('');
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.insertAdjacentHTML('beforeend', markup);
   simpleLightBox.refresh();
 }
 
@@ -110,44 +111,35 @@ const simpleLightBox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-function notification(length, totalHits) {
-    if (length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-  
-      refs.btnLoadMore.style.display = 'none';
-      return;
-    }
-  
-    if (page === 1) {
-      refs.btnLoadMore.style.display = 'flex';
-  
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-    }
-  
-    if (length < 40) {
-      refs.btnLoadMore.style.display = 'none';
-  
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
+
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+async function onLoadMore() {
+  page += 1;
+  try {
+    const response = await fetchArticles(search, page);
+    images = [...images, ...response.data.hits];
+    appendItems(response.data.hits);
+    scroll();
+    showBtn(response.data.totalHits);
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
   }
-  
-  function validateInput() {
-    const searchValue = refs.input.value.trim();
-  
-    if (searchValue === '') {
-      refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку loadMore
-      return;
-    }
-  
-    refs.form.addEventListener('submit', e => {
-      e.preventDefault();
-      
-    });
-  }
-  
-  validateInput();
-  
+}
+
+function cleanGallery() {
+  gallery.innerHTML = '';
+  page = 1;
+  loadMoreBtn.setAttribute('hidden', true);
+}
+
+function scroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
